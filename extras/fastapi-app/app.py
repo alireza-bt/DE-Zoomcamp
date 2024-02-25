fab7d9ce771e15e1e89ce235d51218a7f1bcb25f
alireza-bt <alirezabitarafan73@yahoo.com>
2024-02-25 11:28:52 +0000

    added fastapi-app

---
+++ extras/fastapi-app/app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+import oracledb
+import os
+import logging
+import getpass
+
+PORT = 8000
+
+un = os.environ.get('PYTHON_USERNAME')
+cs = os.environ.get('PYTHON_CONNECTSTRING')
+pw = getpass.getpass(f'Enter password for {un}@{cs}: ')
+
+app = FastAPI()
+
+# Create a connection pool
+pool = oracledb.create_pool(user=un, password=pw, dsn=cs, min=1, max=4, increment=1)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World - from Ali"}
+
+# export PYTHON_CONNECTSTRING AND PYTHON_USERNAME
+
+# Endpoint to retrieve an order by ID
+@app.get("/orders/{order_id}")
+def get_order(order_id: int):
+    try:
+        with pool.acquire() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT order_id, product_name, quantity FROM orders", (order_id,))
+                result = cursor.fetchone()
+                if result:
+                    order = {
+                        "order_id": result[0],
+                        "product_name": result[1],
+                        "quantity": result[2],
+                    }
+                    return order
+                else:
+                    raise HTTPException(status_code=404, detail="Order not found")
+    except oracledb.Error as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {e}")
